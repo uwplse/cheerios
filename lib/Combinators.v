f9a8e9332f68d4d65584d5b14645087ffa2262f2
Keith Simmons <keith@the-simmons.net>
2016-03-08 20:40:44 -0800

    Working on combinators.

--- lib/Combinators.v
+++ lib/Combinators.v
@@ -2,55 +2,54 @@ Require Import StructTactics.
 Require Import List.
 Require Import Types.
 
-SearchAbout Serializer.
-
-Eval cbv in serialize Bool_Serializer true.
-
 Definition pair_serialize
-           (A B: Type)
-           (aSerializer: Serializer A)
-           (bSerializer: Serializer B)
+           {A B: Type}
+           {aSerializer: Serializer A}
+           {bSerializer: Serializer B}
            (p: A*B) :=
   let (a, b) := p in
-  (serialize A) aSerializer ++ (serialize bSerializer b).
+  (serialize a) ++ (serialize b).
 
 Definition pair_deserialize
-         {A:Type}
-         {serialize_a: A -> list bool}
-         {deserialize_a: list bool -> option (A * list bool)}
-         {SA: Serializer serialize_a deserialize_a}
-         {B:Type}
-         {serialize_b: B -> list bool}
-         {deserialize_b: list bool -> option (B * list bool)}
-         {SB: Serializer serialize_b deserialize_b}
-         (bin : list bool) : option ((A*B) * list bool) :=
-  match deserialize_a bin with
+           {A B: Type}
+           {aSerializer: Serializer A}
+           {bSerializer: Serializer B}
+           (bin: list bool) :=
+  match (@deserialize A aSerializer bin) with
     | None => None
     | Some (a, rest) =>
-  match deserialize_b rest with
+  match (@deserialize B bSerializer rest) with (* Is there a better way to have coq figure this out? *)
     | None => None
     | Some (b, remainder) =>
       Some ((a, b), remainder)
   end
   end.
 
-Instance Pair_Serializer: Serializer pair_serialize pair_deserialize.
-split; intros.
-- destruct x.
-  unfold pair_serialize. unfold pair_deserialize. simpl.
-  reflexivity.
-- unfold pair_deserialize in H.
-  repeat break_match; inversion H.
-  + subst.
-    unfold pair_serialize.
-    unfold bool_serialize. simpl.
-    unfold bool_deserialize in Heqo0.
-    break_match; inversion Heqo0.
-    subst.
-    unfold bool_deserialize in Heqo.
-    break_match; inversion Heqo.
-    subst.
-    reflexivity.
-Qed.
+Instance Pair_Serializer: forall (A B: Type), Serializer (A * B) :=
+  {
+    serialize := pair_serialize;
+    deserialize := pair_deserialize;
+  }.
 
-Compute pair_serialize (true, false).
+
+(* Check pair_deserialize. *)
+
+(* Lemma pair_serialize_reversible: forall {A B: Type} *)
+(*                                    {aSerializer: Serializer A} *)
+(*                                    {bSerializer: Serializer B} *)
+(*                                    (x: (A * B)) *)
+(*                                    (bin: list bool), *)
+(*                                    pair_deserialize ((pair_serialize x) ++ bin) = Some (x, bin). *)
+(*   intros. *)
+(*   destruct aSerializer. *)
+(*   destruct bSerializer. *)
+(*   unfold pair_serialize. *)
+(*   break_match. *)
+(*   unfold pair_deserialize. *)
+(*   rewrite app_assoc_reverse. *)
+(*   unfold Types.serialize. *)
+(*   unfold Types.deserialize. *)
+(*   rewrite Serialize_reversible. *)
+(*   rewrite Serialize_reversible0. *)
+(*   reflexivity. *)
+(* Qed. *)
--- lib/Types.v
+++ lib/Types.v
@@ -1,12 +1,12 @@
 Require Import List.
 
-Class Serializer A := {
+Class Serializer A :=
+  {
     serialize: A -> (list bool);
     deserialize: list bool -> option (A * list bool);
-    Serialize_reversible: forall x: A,
-                            deserialize (serialize x) = Some (x, nil) }.
-
-Print serialize.
+    Serialize_reversible: forall (x: A) (bin: list bool),
+                            deserialize ((serialize x) ++ bin) = Some (x, bin)
+  }.
 
 Definition bool_serialize (a: bool) : list bool :=
   a :: nil.
@@ -17,8 +17,8 @@ Definition bool_deserialize (bin : list bool) : option (bool * list bool) :=
     | h :: t => Some (h, t)
   end.
 
-Lemma bool_serialize_reversible: forall x: bool,
-                                   bool_deserialize (bool_serialize x) = Some (x, nil).
+Lemma bool_serialize_reversible: forall (x: bool) (bin: list bool),
+                                   bool_deserialize ((bool_serialize x) ++ bin) = Some (x, bin).
   intros; simpl.
   reflexivity.
 Qed.
@@ -26,6 +26,8 @@ Qed.
 Instance Bool_Serializer: Serializer bool :=
   {
     serialize := bool_serialize;
-    deserialize := bool_deserialize;
-    Serialize_reversible := bool_serialize_reversible
+    deserialize := bool_deserialize
   }.
+intros; simpl.
+reflexivity.
+Qed.
