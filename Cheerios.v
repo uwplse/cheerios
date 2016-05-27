1f30f9fcd23306c59cd339ff76205528e884608b
James Wilcox <jrw12@cs.washington.edu>
2016-05-26 17:59:13 -0700

    do not export notations since they might clash

--- Cheerios.v
+++ Cheerios.v
@@ -1,6 +1,5 @@
 Require Export Cheerios.Core.
 Require Export Cheerios.DeserializerMonad.
-Export DeserializerNotations.
 Require Export Cheerios.Tactics.
 Require Export Cheerios.BasicSerializers.
 Require Export Cheerios.Combinators.
