ff90c51e7422153fb0c12d3c0e50dfda492fef64
Karl Palmskog <palmskog@gmail.com>
2017-01-24 19:06:13 -0600

    added travis config

---
+++ .travis-ci.sh
@@ -0,0 +1,13 @@
+set -ev
+
+opam init --yes --no-setup
+eval $(opam config env)
+
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
+
+opam pin add coq $COQ_VERSION --yes --verbose
+
+opam install StructTact --yes --verbose
+
+./build.sh
