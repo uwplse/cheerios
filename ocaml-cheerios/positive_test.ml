6a0d1743cd18d8394d6717226ec92a0d2de5fdb0
Justin Adsuara <justbads@cs.washington.edu>
2017-06-23 16:09:31 -0700

    Makefile now runs perl script on extracted code, small start on benchmarking

--- ocaml-cheerios/positive_test.ml
+++ ocaml-cheerios/positive_test.ml
@@ -19,6 +19,16 @@ let test_positive p =
   Printf.printf "success\n"
 ;;
 
+let make_positive n =
+  let rec aux n flag k =
+    if n = 0
+    then k XH
+    else aux (n - 1) (not flag) (if flag
+                                 then fun p -> XI (k p)
+                                 else fun p -> XO (k p)) in
+  aux n true (fun p -> p)
+;;
+  
 let _ = test_positive XH;
         test_positive (XI XH);
         test_positive (XO XH);
@@ -26,3 +36,38 @@ let _ = test_positive XH;
         test_positive (XO (XI XH));
         test_positive (XI (XI (XO (XO XH))));
         test_positive (XI (XI (XO (XI (XO (XO (XO XH)))))));;
+
+(* benchmarking *)
+let time_cheerios_serialize_once (p : positive) : float =
+  let start = Sys.time () in
+  let _ = serialize_positive p in
+  let stop = Sys.time () in
+  stop -. start
+;;
+
+let rec time_cheerios_loop max =
+  let interval = 50 in
+  let rec loop n = 
+    if n = max
+    then ()
+    else Printf.printf "Time for size %d: %f"
+                       n
+                       (time_cheerios_serialize_once (make_positive (n * interval)));
+    loop (n + 1) in
+  loop 0
+;;
+
+
+(* 
+  
+let marshalled =
+  Marshal.to_bytes XH []
+;;
+
+let p = Marshal.from_bytes marshalled 0;;
+let _ = print_positive p;;
+ *)
+
+let _ = Printf.printf "Time: %f\n" (time_cheerios_serialize_once
+                                      (make_positive 100000))
+;;
