2883e8a01c91e41e15e8fbeee05130dc02228428
Justin Adsuara <justbads@cs.washington.edu>
2017-07-09 23:31:46 -0700

    Added primitives interface

---
+++ ocaml-cheerios/serializer_primitives.mli
@@ -0,0 +1,26 @@
+type serializer
+type 'a deserializer
+type wire = bytes
+type ('s, 'a) fold_state =
+  | Done of 'a
+  | More of 's
+  | Error
+;;
+
+(* serializer *)
+val empty : serializer
+val putByte : char -> serializer
+val append : serializer -> serializer -> serializer
+
+  (* deserializer *)
+val getByte : char deserializer
+val bind : 'a deserializer -> ('a -> 'b deserializer) -> 'b deserializer
+val ret : 'a -> 'a deserializer
+val fail : 'a deserializer
+val map : ('a -> 'b) -> 'a deserializer -> 'b deserializer
+val fold : (char -> 's -> ('s, 'a) fold_state) -> 's -> 'a deserializer
+  
+(* wire *)
+val wire_wrap : serializer -> wire
+val size : wire -> int
+val deserialize_top : 'a deserializer -> wire -> 'a option
