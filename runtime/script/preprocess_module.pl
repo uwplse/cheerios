56a42d06a546566cdf0c5c7fcd8fda23842c0c41
Justin Adsuara <justbads@cs.washington.edu>
2017-07-14 19:12:50 -0700

    OCaml plus build stuff for extracted tree serializer

---
+++ runtime/script/preprocess_module.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# https://perlmaven.com/how-to-replace-a-string-in-a-file-with-perl
+
+my $serializer_name = $ARGV[0];
+my $mli_name = $serializer_name . '.mli';
+my $ml_name = $serializer_name . '.ml';
+
+my $ml = read_file($ml_name);
+$ml =~ s/type wire\n/type wire = bytes\n/g;
+$ml =~ s/(\w|\.)+.RWClass\.deserialize_top/Serializer_primitives.deserialize_top/g;
+write_file($ml_name, $ml);
+
+my $mli = read_file($mli_name);
+$mli =~ s/type wire\n/type wire = bytes\n/g;
+write_file($mli_name, $mli);
+exit;
+
+sub read_file {
+    my ($filename) = @_;
+    
+    open my $in, '<:encoding(UTF-8)', $filename or die "Could not open '$filename' for reading $!";
+    local $/ = undef;
+    my $all = <$in>;
+    close $in;
+    
+    return $all;
+}
+
+sub write_file {
+    my ($filename, $content) = @_;
+    
+    open my $out, '>:encoding(UTF-8)', $filename or die "Could not open '$filename' for writing $!";;
+    print $out $content;
+    close $out;
+    
+    return;
+}
--- runtime/test/tree_test.ml
+++ runtime/test/tree_test.ml
@@ -14,8 +14,8 @@ let test_width max_height width =
   let rec loop i =
     if i < max_height
     then (test_serialize_deserialize (make false i width)
-                                     tree_serialize_top
-                                     (fun w -> match tree_deserialize_top w with
+                                     tree_serialize_iostream_top
+                                     (fun w -> match tree_deserialize_iostream_top w with
                                                | Some p -> p
                                                | None -> failwith "Deserialization failed")
                                      (fun _ -> Printf.printf "height %d, width %d"
@@ -34,7 +34,7 @@ let space_main () =
   let max_height = 10 in
   let rec loop i =
     if i < max_height
-    then (compare_cheerios_marshal_space (fun n -> make false n 2) (tree_serialize_top) i;
+    then (compare_cheerios_marshal_space (fun n -> make false n 2) (tree_serialize_iostream_top) i;
           loop (i + 1))
   in
   print_string "\nSpace usage tests...\n";
@@ -43,9 +43,9 @@ let space_main () =
 let bench_main () =
   print_string "\nBenchmarks...\n";
   compare_time_loop (fun n -> make false n 2)
-                    40 1 2
-                    tree_serialize_top
-                    (fun w -> match tree_deserialize_top w with
+                    20 1 20
+                    tree_serialize_iostream_top
+                    (fun w -> match tree_deserialize_iostream_top w with
                               | Some p -> p
                               | None -> failwith "Deserialization failed")
                     
@@ -54,29 +54,8 @@ let avg_time_height (f : 'a tree -> 'b) (h : int) =
   let num_tries = 100 in
   avg (time_loop (fun n -> make false n 2) h num_tries f)
 
-let compare_map_main () =
-  let max_height = 25 in
-  let rec loop i =
-    if i < max_height
-    then let map_avg = avg_time_height (map0 (fun _ -> ())) i in
-         let tr_map_avg = avg_time_height (tree_map' (fun _ -> ())) i in
-         (Printf.printf "map: %f, tail-rec map: %f \n%!" map_avg tr_map_avg;
-          loop (i + 1)) in
-  loop 0
-
-let compare_serialize_tr_main () =
-  let max_height = 25 in
-  let rec loop i =
-    if i < max_height
-    then let s_avg = avg_time_height (fun t -> t) i in
-         let tr_s_avg = avg_time_height tree_serialize_top' i in
-         (Printf.printf "height: %d, serialize: %f, tail-rec serialize: %f \n%!"
-                        i s_avg tr_s_avg;
-          loop (i + 1)) in
-  loop 0
-                    
 let main () = test_main ();
               space_main ();
               bench_main ()
 
-let _ = compare_serialize_tr_main ()
+let _ = main ()
