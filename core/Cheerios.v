06c8f53e45cde91477fae34e406fc66956fa8463
Justin Adsuara <justbads@cs.washington.edu>
2017-07-20 16:44:02 -0700

    Update Cheerios.v

--- core/Cheerios.v
+++ core/Cheerios.v
@@ -3,4 +3,5 @@ Require Export Cheerios.DeserializerMonad.
 Require Export Cheerios.Tactics.
 Require Export Cheerios.BasicSerializers.
 Require Export Cheerios.Combinators.
-
+Require Export Cheerios.Tactics.
+Require Export Cheerios.Types.
