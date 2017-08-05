1c49f6332ef0dbe28a7e6600812a5fe3c6465cd2
Karl Palmskog <palmskog@gmail.com>
2017-08-04 22:53:33 -0500

    no float extraction anymore

--- extraction/ExtrOcamlCheeriosFloat.v
+++
@@ -1,4 +0,0 @@
-Require Import Cheerios.Cheerios.
-
-Extract Inlined Constant float_serialize => "Serializer_primitives.putFloat".
-Extract Inlined Constant float_deserialize => "Serializer_primitives.getFloat".
