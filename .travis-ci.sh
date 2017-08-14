620790e9cd5b75713aa08244d01d14d15e5c1693
Karl Palmskog <palmskog@gmail.com>
2017-08-13 21:11:30 -0500

    adjust travis for 8.7

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,6 +4,7 @@ opam init --yes --no-setup
 eval $(opam config env)
 
 opam repo add coq-released https://coq.inria.fr/opam/released
+opam repo add coq-core-dev https://coq.inria.fr/opam/core-dev
 opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
 
 opam pin add coq $COQ_VERSION --yes --verbose
