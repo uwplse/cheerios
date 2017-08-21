4707ffe5457c90833429137a54571586d309c95c
Karl Palmskog <palmskog@gmail.com>
2017-08-21 17:22:27 -0500

    expose wire type in module type

--- core/Types.v
+++ core/Types.v
@@ -31,9 +31,11 @@ Inductive byte :=
 | xea | xeb | xec | xed | xee | xef | xf0 | xf1 | xf2 | xf3 | xf4 | xf5 | xf6
 | xf7 | xf8 | xf9 | xfa | xfb | xfc | xfd | xfe | xff.
 
+Definition wire := list byte.
+
 Module Type WRITER.
   Parameter t : Type.
-  Parameter wire : Type.
+  Definition wire := wire.
   Parameter wire_eq_dec : forall w w' : wire, {w = w'}+{w <> w'}.
 
   Parameter empty : t.
